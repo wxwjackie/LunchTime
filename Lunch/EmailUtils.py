a52e1b3d50fca1f310b9d4d8ed242c44208780c5
lowang <lowang@infinera.com>
2016-12-16 12:41:22 +0800

    -a

--- Lunch/EmailUtils.py
+++ Lunch/EmailUtils.py
@@ -43,9 +43,14 @@ def notify_order_state_to_user(mail_to, user, order_state):
     subjuct = "Your order state is [%s]" % order_state
     
     content = "Dear %s,<br/><br/>" % user
+    wel_str = """
+            Welcome to use the automatic Meal Order System.
+            Vist the website http://10.220.43.58:8000/lunch/ to have a try.
+    """
 
     if order_state == "Submitted":
-        content = content + "Thanks for submitting the meal."
+#         content = content + "Thanks for submitting the meal."
+        content = content + wel_str
     elif order_state == "Canceled":
         content = content + "Your meal order has been canceled."
     elif order_state == "Confirmed":
